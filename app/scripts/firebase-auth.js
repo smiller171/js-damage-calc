fbc93da7c7e3d44aa64e166d7fcb8c5b01c33b9a
Scott Miller <scott.miller171@gmail.com>
2016-09-30 20:52:13 -0400

    get auth working

---
+++ app/scripts/firebase-auth.js
@@ -0,0 +1,55 @@
+// FirebaseUI config.
+var uiConfig = {
+    'signInSuccessUrl': '',
+    'signInFlow': 'popup',
+    'signInOptions': [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    'tosUrl': '<your-tos-url>',
+    'callbacks': {
+        'signInSuccess': function(currentUser, credential, redirectUrl) {
+            // Do something.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+        }
+    }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+
+initApp = function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            user.getToken().then(function(accessToken) {
+                $("#userImg").attr('src', photoURL);
+                $("#username").text("Hi " + displayName)
+                $('#userInfo').show();
+                $('#firebaseui-auth-container').hide();
+            });
+        } else {
+            // User is signed out.
+            $('#firebaseui-auth-container').show();
+            $('#userInfo').hide();
+        }
+    }, function(error) {
+        console.log(error);
+    });
+};
+
+window.addEventListener('load', function() {
+    initApp()
+});
